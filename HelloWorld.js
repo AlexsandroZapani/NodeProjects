b712a30c94b53ef3bf10d304cebf6bd5b93e28b9
AlexsandroZapani <alexsandrozapani1@gmail.com>
2024-05-23 23:55:55 -0300

    feat: Anotações basicas

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+console.log('Este código sera executado via REPL do node.js');
+
+// Execute desta forma em seu CMD.
+// node "\dsv-git\Node Projects\HelloWorld.js"
+// console é o bjeto qual disponibiliza a função log
---
+++ MartinFowler.js
@@ -0,0 +1,2 @@
+console.log('Any fool can write code that a computer can undersatand.');
+console.log('Good programers write code that humans can unsderstand.');
