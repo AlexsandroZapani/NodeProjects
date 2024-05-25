5ac25e7a26349dd1fb5d463e118b80192c6056eb
AlexsandroZapani <alexsandrozapani1@gmail.com>
2024-05-25 15:58:18 -0300

    Feat: Reset

--- HelloWorld.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello World');
-console.log('Este código sera executado via REPL do node.js');
-
-// Execute desta forma em seu CMD.
-// node "\dsv-git\Node Projects\HelloWorld.js"
-// console é o bjeto qual disponibiliza a função log
--- string.js
+++
@@ -1,8 +0,0 @@
-let str ='Teste';
-console.log(str.length);
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-str = '1';
-console.log(parseInt(str));
-str = '1.5';
-console.log(parseFloat(str));
